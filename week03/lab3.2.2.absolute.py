f017062ef04a4a9ccbdcd0b4019a9949a8b13082
Rahul <rahuldewan27@gmail.com>
2024-02-08 18:47:30 +0000

    'week03'

---
+++ week03/lab3.2.2.absolute.py
@@ -0,0 +1,8 @@
+# lab3.2.2.absolute.py
+# Input a number and return the absolute value for the same
+# author : rahul parvesh dewan
+
+input_number = float(input("Please enter a number: "))
+absolute_number = abs(input_number)
+
+print("The absolute number of {} is {}.".format(input_number, absolute_number))
---
+++ week03/lab3.2.3.floor.py
@@ -0,0 +1,10 @@
+# lab3.2.3.floor.py
+# floors an inputted number to an intenger
+# author = rahul parvesh dewan
+
+import math
+
+number_to_floor = float(input("Enter a float number: "))
+floored_number = math.floor(number_to_floor)
+
+print("{} floored is {}".format(number_to_floor, floored_number))
